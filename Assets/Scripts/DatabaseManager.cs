bd7203589f4f6d417be5878b3c4e5ee2fce32302
TaitotaloSanteri <santeri.hakoniemi@edu.taitotalo.fi>
2021-01-19 08:48:50 +0200

    Initial commit

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Reflection;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private static SQLiteConnection dbConnection;
+    private static SQLiteCommand dbCommand;
+    private const string dbName = "TestDatabase.db";
+
+
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void InitializeDatabase()
+    {
+        // Varmistetaan, että ei luoda kuin yksi yhteys tietokantaan.
+        if (dbConnection != null)
+        {
+            Debug.Log("Tietokantayhteys on jo olemassa.");
+            return;
+        }
+        // Määritetään tietokannan tiedoston sijainti.
+        string dbPath = $"Data Source={Application.persistentDataPath}/{dbName}";
+        // Luodaan yhteys tietokantaan. Jos tiedostoa ei ole olemassa,
+        // tämä komento luo uuden tiedoston.
+        dbConnection = new SQLiteConnection(dbPath);
+        // Avataan yhteys tietokantaan.
+        dbConnection.Open();
+        // Luodaan yhteys SQL-komentojen ja juuri luodun tietokannan välille.
+        dbCommand = new SQLiteCommand(dbConnection);
+        Debug.Log("Yhteys tietokantaan muodostettu. " + dbPath);
+    }
+}
---
+++ Assets/Scripts/HighscoreManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighscoreManager : MonoBehaviour
+{
+}
+
+public class Highscore
+{
+    public string name;
+    public int score;
+    public string date;
+}
